411e0a3bc4117ebad6789415c143ba257fb1e224
Bruna de Paiva Gonçalves <65919877+Pip0ca@users.noreply.github.com>
2020-07-14 15:42:56 -0300

    Adicionado analise do resultado imc

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ function calcular() {
   let peso = document.getElementById("peso");
   var resultado = peso.value / (altura.value ^ 2);
   var showResultado = document.getElementById("resultado");
+  var showAnalise = document.getElementById("analise");
 
   if (isNaN(resultado)) {
     showResultado.innerText = "Favor insira um número";
@@ -11,15 +12,35 @@ function calcular() {
   } else {
     showResultado.innerText = `${resultado.toFixed(2)}`;
     aplicaEstilo(showResultado);
+    
   }
-}
+
+  if(resultado<18.5){
+    showAnalise.innerText = "Você está abaixo do peso";
+    }
+    else if(resultado>=18.5 && resultado<24.9){
+      showAnalise.innerText = "Seu peso está ideal";
+    }
+    
+    else if(resultado>=25 && resultado<29.9) {
+      showAnalise.innerText = "Você está com Obesidade grau I";
+    }
+    else if(resultado>=30 && resultado<39.9) {
+      showAnalise.innerText = "Você está com Obesidade grau II";
+    }
+    else if (resultado>40)
+    showAnalise.innerText = "Você está com Obesidade grau III";
+    
+    }
+
 
 function aplicaEstilo(componente) {
-  componente.style.backgroundColor = "#131313";
+  componente.style.backgroundColor = "#ffffff";
   componente.style.border = "none";
   componente.style.borderRadius = "10px";
-  componente.style.color = "#fff";
+  componente.style.color = "#000000";
   componente.style.padding = "10px";
   componente.style.textAlign = "center";
   componente.style.width = "100px";
+  componente.style.height ="40px";
 }
