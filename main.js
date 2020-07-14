257e182a7caacdec62383e1a03019b2c262b2416
Bruna de Paiva Gonçalves <65919877+Pip0ca@users.noreply.github.com>
2020-07-14 16:20:21 -0300

    ajusre texto analise

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ function calcular() {
   let altura = document.getElementById("altura");
   let peso = document.getElementById("peso");
   var resultado = peso.value / (altura.value ^ 2);
-  var showResultado = document.getElementById("resultado");
+  var showResultado = document.getElementById("valor");
   var showAnalise = document.getElementById("analise");
 
   if (isNaN(resultado)) {
@@ -11,22 +11,25 @@ function calcular() {
     peso.style.border = "1px solid #ea0000";
   } else {
     showResultado.innerText = `${resultado.toFixed(2)}`;
-    aplicaEstilo(showResultado);
-    
+    aplicaEstiloRE(showResultado);  
   }
 
   if(resultado<18.5){
     showAnalise.innerText = "Você está abaixo do peso";
+    
     }
     else if(resultado>=18.5 && resultado<24.9){
       showAnalise.innerText = "Seu peso está ideal";
+      
     }
     
     else if(resultado>=25 && resultado<29.9) {
       showAnalise.innerText = "Você está com Obesidade grau I";
+      
     }
     else if(resultado>=30 && resultado<39.9) {
       showAnalise.innerText = "Você está com Obesidade grau II";
+      
     }
     else if (resultado>40)
     showAnalise.innerText = "Você está com Obesidade grau III";
@@ -34,7 +37,7 @@ function calcular() {
     }
 
 
-function aplicaEstilo(componente) {
+function aplicaEstiloRE(componente) {
   componente.style.backgroundColor = "#ffffff";
   componente.style.border = "none";
   componente.style.borderRadius = "10px";
@@ -43,4 +46,8 @@ function aplicaEstilo(componente) {
   componente.style.textAlign = "center";
   componente.style.width = "100px";
   componente.style.height ="40px";
+}
+
+function aplicaEstiloAN(componente){
+  
 }
